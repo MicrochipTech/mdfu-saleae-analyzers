3e7e17f5c88935dce6c3e3ea9858aa216f7aa5ce
Keith Chamberlain <keith.chamberlain@microchip.com>
2023-09-14 16:50:12 -0400

    Removed unused config options

--- LWFTPCmdAnalyzer/HighLevelAnalyzer.py
+++ LWFTPCmdAnalyzer/HighLevelAnalyzer.py
@@ -9,9 +9,9 @@ import FileTransferCommandsAndResponses as CandR
 # High level analyzers must subclass the HighLevelAnalyzer class.
 class Hla(HighLevelAnalyzer):
     # List of settings that a user can set for this High Level Analyzer.
-    my_string_setting = StringSetting()
-    my_number_setting = NumberSetting(min_value=0, max_value=100)
-    my_choices_setting = ChoicesSetting(choices=('A', 'B'))
+    # my_string_setting = StringSetting()
+    # my_number_setting = NumberSetting(min_value=0, max_value=100)
+    # my_choices_setting = ChoicesSetting(choices=('A', 'B'))
 
     # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
     result_types = {
