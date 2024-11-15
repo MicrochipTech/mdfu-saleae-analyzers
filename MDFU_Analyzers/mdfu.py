9607e77345f5d1a52f1695db2097f2a1db8129f4
Lars Häring <lars.haring@microchip.com>
2024-11-15 16:49:05 +0100

    MDFU-6: License and typo fix.

--- MDFU_Analyzers/mdfu.py
+++ MDFU_Analyzers/mdfu.py
@@ -1,3 +1,16 @@
+# Copyright 2024 Microchip Technology Incorporated
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """MDFU protocol"""
 from enum import Enum
 from packaging.version import Version
@@ -419,8 +432,8 @@ class ClientInfo():
 MDFU client information
 --------------------------------
 - MDFU protocol version: {self.protocol_version}
-- Number of command buffers: {self.buffer_count} bytes
-- Maximum packet data length: {self.buffer_size}
+- Number of command buffers: {self.buffer_count}
+- Maximum packet data length: {self.buffer_size} bytes
 {itd_txt}
 Command timeouts
 - Default timeout: {self.default_timeout} seconds
